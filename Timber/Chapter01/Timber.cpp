fdfc717332039bed15114b64756f224d05c45030
harrisonhu66 <harrisonhu66@gmail.com>
2025-10-28 12:25:29 +0800

    Open a window using SFML with Window abstracting 2D drawing and VideoMode abstracting resolution stuff

--- Timber/Chapter01/Timber.cpp
+++ Timber/Chapter01/Timber.cpp
@@ -1,69 +1,18 @@
 // Include important libraries here
+// Abstract 2D graphics like sprites, texts, shapes, ...
 #include <SFML/Graphics.hpp>
 
 // Make code easier to type with "using namespace" 
-using namespace sf;
+// This is a bad practice, just use using-declartion or qualified names
+// It introduces names into the LCA scope of the target scope and current scope (which is the global scope)
+// https://quuxplusone.github.io/blog/2020/12/21/using-directive/
+//using namespace sf;
 
 // This is where our game starts from int main()
-int main()
-{
-	// Create a video mode object 
-	VideoMode vm(1920, 1080);
-
-	// Create and open a window for the game
-	RenderWindow window(vm, "Timber!!!"/*, Style::Fullscreen*/);
-
-	// Create a texture to hold a graphic on the GPU 
-	Texture textureBackground;
-
-	// Load a graphic into the texture 
-	textureBackground.loadFromFile("graphics/background.png");
-
-	// Create a sprite 
-	Sprite spriteBackground;
-
-	// Attach the texture to the sprite 
-	spriteBackground.setTexture(textureBackground);
-
-	// Set the spriteBackground to cover the screen spriteBackground.setPosition(0,0);
-
-
-	while (window.isOpen())
-	{
-
-		/*
-		**************************************** 
-		Handle the players input
-		****************************************
-		*/
-
-		if (Keyboard::isKeyPressed(Keyboard::Escape))
-		{
-			window.close();
-		}
-
-		/*
-		**************************************** 
-		Update the scene
-		****************************************
-		*/
-
-
-		/*
-		**************************************** 
-		Draw the scene
-		****************************************
-		*/
-
-		// Clear everything from the last frame 
-		window.clear();
-
-		// Draw our game scene here
-		window.draw(spriteBackground);
-
-		// Show everything we just drew 
-		window.display();
-	}
+// OS calls main() and receive a value indicating this program success or not.
+int main() {
+	sf::VideoMode video_mode{1920, 1080};
+	sf::RenderWindow window{video_mode, "Timber!!!"};
 	return 0;
 }
 
