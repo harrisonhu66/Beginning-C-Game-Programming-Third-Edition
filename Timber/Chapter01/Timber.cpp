823f640514313003855b9f78e449c18e7c62bc74
harrisonhu66 <harrisonhu66@gmail.com>
2025-10-28 16:17:25 +0800

    Add static clouds, a bee, and a tree

--- Timber/Chapter01/Timber.cpp
+++ Timber/Chapter01/Timber.cpp
@@ -17,9 +17,38 @@ int main() {
 	sf::Texture background_texture{}; 
 	// load the texure into the video memory
 	background_texture.loadFromFile("graphics/background.png"); 
-
 	sf::Sprite background_sprite{background_texture};
 	background_sprite.setPosition(0, 0);
+
+	// REFACTOR 重复出现load texture from a path, create a correspoding sprite a given localtion
+	// auto sprite = SpriteRegistry::Create({x, y}, <path or some identity(int32, string, ...) pointing to a specific texture>);
+	// identity-address the texture, share them among spirtes.
+	sf::Texture tree_texture{};
+	tree_texture.loadFromFile("graphics/tree.png");
+	sf::Sprite tree_sprite{tree_texture};
+	tree_sprite.setPosition(810, 0); // 1920/2=810
+
+	sf::Texture bee_texture{};
+	bee_texture.loadFromFile("graphics/bee.png");
+	sf::Sprite bee_sprite{bee_texture};
+	bee_sprite.setPosition(0, 800);
+	bool bee_active{false};
+	float bee_speed{0.0f}; // pixels per second
+
+	sf::Texture cloud_texture{};
+	cloud_texture.loadFromFile("graphics/cloud.png");
+	sf::Sprite cloud_sprite1{cloud_texture};
+	sf::Sprite cloud_sprite2{cloud_texture};
+	sf::Sprite cloud_sprite3{cloud_texture};
+	cloud_sprite1.setPosition(0, 0);
+	cloud_sprite2.setPosition(0, 250);
+	cloud_sprite3.setPosition(0, 500);
+	bool cloud_active1{false};
+	bool cloud_active2{false};
+	bool cloud_active3{false};
+	float cloud_speed1{0.0f};
+	float cloud_speed2{0.0f};
+	float cloud_speed3{0.0f};
 
 	while (window.isOpen()) {
 		// Input
@@ -32,9 +61,17 @@ int main() {
 		// Draw
 		window.clear();
 
+		// Painter's algorithms indicates that the order of drawing is important
+		// https://en.wikipedia.org/wiki/Painter%27s_algorithm
+
 		// 这里的GPU流水线应该还有更多的信息, 但是先抓住重点
 		// put the texture to some display buffer (e.g. framebuffer)
 		window.draw(background_sprite); 
+		window.draw(cloud_sprite1);
+		window.draw(cloud_sprite2);
+		window.draw(cloud_sprite3);
+		window.draw(tree_sprite);
+		window.draw(bee_sprite);
 
 		window.display(); // Double-buffering switching/flipping
 	}
