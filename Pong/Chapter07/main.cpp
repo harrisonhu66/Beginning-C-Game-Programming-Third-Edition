5743809e6393afbde34e4d19ab581b5f062be61f
harrisonhu66 <harrisonhu66@gmail.com>
2025-11-11 08:28:14 +0800

    Add bat controll

--- Pong/Chapter07/main.cpp
+++ Pong/Chapter07/main.cpp
@@ -1,134 +1,73 @@
-﻿#include "Bat.h"
-#include <sstream>
-#include <cstdlib>
-#include <SFML/Graphics.hpp>
-#include "Ball.h"
+﻿#include <SFML/Graphics.hpp>
+
+class Bat {
+public:
+    Bat(float x, float y) {
+        this->shape.setPosition(x, y);
+    }
+
+    void update(sf::Time dt) {
+        auto position = this->shape.getPosition();
+        if (wanted_to_move_right) {
+            position.x += dt.asSeconds() * this->horizontal_speed;
+        }
+        if (wanted_to_move_left) {
+            position.x -= dt.asSeconds() * this->horizontal_speed;
+        }
+        this->shape.setPosition(position);
+    }
+
+    const sf::RectangleShape& get_shape() const {
+        return this->shape;
+    }
+
+    bool wanted_to_move_left{};
+    bool wanted_to_move_right{};
+private:
+    sf::RectangleShape shape{{50, 5}};
+    float horizontal_speed{1000.0f};
+};
+
 int main()
 {
-    // Create a video mode object
-    VideoMode vm(1920, 1080);
-    // Create and open a window for the game
-    RenderWindow window(vm, "Pong"/*, Style::Fullscreen*/);
-    int score = 0;
-    int lives = 3;
+    sf::VideoMode vm{1920, 1080};
+    sf::RenderWindow window{vm, "Pong"};
+    
+    Bat bat{1920 / 2, 1080 - 20};
 
-    // Create a bat at the bottom center of the screen
-    Bat bat(1920 / 2, 1080 - 20);
-    // We will add a ball in the next chapter
-    Ball ball(1920 / 2, 0);
-    // Create a Text object called HUD
-    Text hud;
-    // A cool retro-style font
-    Font font;
-    font.loadFromFile("fonts/DS-DIGIT.ttf");
-    // Set the font to our retro-style
-    hud.setFont(font);
-    // Make it nice and big
-    hud.setCharacterSize(75);
-    // Choose a color
-    hud.setFillColor(Color::White);
-    hud.setPosition(20, 20);
-    // Here is our clock for timing everything
-    Clock clock;
+    sf::Clock clock{};
     while (window.isOpen())
     {
-        /*
-        Handle the player input
-        ****************************
-        ****************************
-        ****************************
-        */
-        Event event;
-        while (window.pollEvent(event))
-        {
-            if (event.type == Event::Closed)
-                // Quit the game when the window is closed
+        sf::Event event{};
+        while (window.pollEvent(event)) {
+            if (event.type == sf::Event::Closed) {
                 window.close();
-        }
-        // Handle the player quitting
-        if (Keyboard::isKeyPressed(Keyboard::Escape))
-        {
-            window.close();
-        }
-        // Handle the pressing and releasing of the arrow keys
-        if (Keyboard::isKeyPressed(Keyboard::Left))
-        {
-            bat.moveLeft();
-        }
-        else
-        {
-            bat.stopLeft();
-        }
-        if (Keyboard::isKeyPressed(Keyboard::Right))
-        {
-            bat.moveRight();
-        }
-        else
-        {
-            bat.stopRight();
-        }
-
-
-        /*
-        Update the bat, the ball and the HUD
-        *****************************
-        *****************************
-        *****************************
-        */
-        // Update the delta time
-        Time dt = clock.restart();
-        bat.update(dt);
-        ball.update(dt);
-        // Update the HUD text
-        std::stringstream ss;
-        ss << "Score:" << score << "  Lives:" << lives;
-        hud.setString(ss.str());
-
-        // Handle ball hitting the bottom
-        if (ball.getPosition().top > window.getSize().y)
-        {
-            // reverse the ball direction
-            ball.reboundBottom();
-            // Remove a life
-            lives--;
-            // Check for zero lives
-            if (lives < 1) {
-                // reset the score
-                score = 0;
-                // reset the lives
-                lives = 3;
             }
         }
 
-        // Handle ball hitting sides
-        if (ball.getPosition().left < 0 ||
-            ball.getPosition().left + ball.getPosition().width> window.getSize().x)
-        {
-            ball.reboundSides();
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Escape)) {
+            window.close();
         }
 
-        // Has the ball hit the bat?
-        if (ball.getPosition().intersects(bat.getPosition()))
-        {
-            // Hit detected so reverse the ball and score a point
-            ball.reboundBatOrTop();
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left)) {
+            bat.wanted_to_move_left = true;
+        }
+        else {
+            bat.wanted_to_move_left = false;
+        }
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right)) {
+            bat.wanted_to_move_right = true;
+        }
+        else {
+            bat.wanted_to_move_right = false;
         }
 
+        sf::Time dt = clock.restart();
+        bat.update(dt);
 
-        /*
-        Draw the bat, the ball and the HUD
-        *****************************
-        *****************************
-        *****************************
-        */
         window.clear();
-        window.draw(hud);
-        window.draw(bat.getShape());
-        window.draw(ball.getShape());
+        window.draw(bat.get_shape());
         window.display();
-
-
-
     }
     return 0;
 }
